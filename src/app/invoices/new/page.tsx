66199d16ceb90e141b075cec32fc92baf723b763
Vivek Namdev <viveknamdev257@gmail.com>
2025-01-03 20:07:33 +0530

    setup database

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,10 +1,15 @@
+import { sql } from 'drizzle-orm';
+
+import { db } from '@/db';
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
 
-export default function Home() {
+export default async function Home() {
+const results = await db.execute(sql`SELECT current_database()`);
   return (
     <main className="flex flex-col h-full justify-center items-center text-center gap-6 max-w-5xl mx-auto py-6">
       <div className="flex justify-center items-center w-full px-4">
---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, integer, serial, timestamp, pgEnum, text } from "drizzle-orm/pg-core";
+
+export const statusEnum = pgEnum('status', ['open', 'paid', 'void', 'uncollectible']);
+
+export const Invoices = pgTable('invoices', {
+    id: serial('id').primaryKey().notNull(),
+    createTs: timestamp('createTs').defaultNow().notNull(),
+    value: integer('value').notNull(),
+    description: text('description').notNull(),
+    status: statusEnum('status').notNull()
+})
